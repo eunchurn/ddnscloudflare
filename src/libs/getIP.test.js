d0535917480d7e680d7c2ea946ca7218b01dd0e5
Eunchurn Park <eunchurn.park@gmail.com>
2019-12-05 23:46:25 +0900

    expect assertions

--- src/libs/getIP.test.js
+++ src/libs/getIP.test.js
@@ -3,6 +3,6 @@ import getIP from "./getIP";
 jest.mock("ipinfo");
 
 it("getIP with promises", () => {
-  // expect.assertions(1);
+  expect.assertions(1);
   return getIP().then(data => expect(data).toEqual("220.86.118.35"));
 });
