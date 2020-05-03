6cfc4d60a22e6e64e523bb77b4a28fa0cb44c41d
Eunchurn Park (macBookPro) <ec.park@danbicorp.com>
2020-05-04 01:56:25 +0900

    mock added

--- src/libs/getIP.test.js
+++ src/libs/getIP.test.js
@@ -1,10 +1,11 @@
 import getIP from "./getIP";
 
-jest.mock("ipinfo");
+jest.mock("./getIP");
+
+getIP.mockResolvedValue("220.86.118.90");
 
 it("getIP with promises", () => {
-  expect.assertions(1);
   return getIP().then((data) =>
-    expect(data).toEqual("220.86.118.35"),
+    expect(data).toEqual("220.86.118.90"),
   );
 });
